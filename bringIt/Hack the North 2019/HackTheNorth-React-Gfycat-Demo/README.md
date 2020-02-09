5c7e4d2269632b72cbefd88460c2b3cacfd58627
Abdul Haqani <ahaqa011@uottawa.ca>
2020-02-09 02:53:23 -0500

    commit

--- bringIt/Hack the North 2019/HackTheNorth-React-Gfycat-Demo/README.md
+++
@@ -1,2 +0,0 @@
-# gfycat-video-looper-react
-A video looper using HTML 5 canvas and Gfycat API
--- bringIt/Hack the North 2019/proj/src/components/Account/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = () => (
-  <div>
-    <h1>Index</h1>
-  </div>
-);
-
-export default Index;
